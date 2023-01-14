66f75be05c6cd541dcef23cd4d13d88629196cf9
nik1168 <ngeisser32@gmail.com>
2023-01-13 22:00:52 -0400

    feat: add hero initial component

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React, {FC} from 'react';
 import {Cursor, useTypewriter} from 'react-simple-typewriter';
 
@@ -18,10 +19,19 @@ const Hero: FC<HeroProps> = (): JSX.Element => {
   return (
     <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
       <BackgroundCircles />
-      <h1>
-        <span>{text}</span>
-        <Cursor cursorColor="#F7AB0A" />
-      </h1>
+      <Image
+        className="relative rounded-full mx-auto h-32 w-32 mx-auto object-cover"
+        src={'/profile.jpg'}
+        alt="Picture of the author"
+        width={500}
+        height={500}
+      />
+      <div>
+        <h1>
+          <span>{text}</span>
+          <Cursor cursorColor="#F7AB0A" />
+        </h1>
+      </div>
     </div>
   );
 };
